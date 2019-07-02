232ffde9abac57681ef88dbd53be1867bb86aff7
YonThiriAung <yonthiriaung041998@gmail.com>
2019-07-02 14:31:14 +0630

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Yon Thiri Aung on 7/2/19.
+//  Copyright © 2019 Yon Thiri Aung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
